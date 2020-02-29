20a243c3075fb32c010c8daaf67d001e38b4ecc0
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-02-29 13:42:03 -0500

    Finished twitter app without gif

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Ashraf Khan on 2/29/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var tweetContent: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
